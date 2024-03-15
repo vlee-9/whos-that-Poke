9ef5f1130b8f5e026b084d2392259943d655ef01
voktyr <voktyr.soc@gmail.com>
2024-03-14 20:15:00 -0700

    game cycle rotattion + dataset test

--- src/gameset.jsx
+++ src/gameset.jsx
@@ -1,53 +1,51 @@
 import { useState } from 'react'
+import { pokeG1 } from './Gen1Data'
 
 export default function GameSet() {
 
-    const [pokeList, setPokeList] = useState({
+    const [settings, setSettings] = useState({
         active: false,
         pokeArr: [],
         hiddenPoke: {}
     })
-
-    console.log(pokeList.hiddenPoke)
+    
+    const { hiddenPoke, pokeArr } = settings
 
     function loadPokemon() {
-        fetch(`https://pokeapi.co/api/v2/pokemon?limit=150&offset=0`)
-            .then(res => res.json())
-            .then(data => {
 
-                let chosenPoke = []
-                const results = data['results']
-                const dataL = results.length
+        let chosenPoke = []
+        const dataL = pokeG1.length
 
-                for (let i = 0; i < 4; i++) {
-                    let x = results[Math.floor(Math.random() * dataL)]
-                    chosenPoke.push(x)
-                }
+        for (let i = 0; i < 4; i++) {
+            let x = pokeG1[Math.floor(Math.random() * dataL)]
+            chosenPoke.push(x)
+        }
 
 
-                let x = chosenPoke[Math.floor(Math.random() * 4)]
-                fetch(`${x.url}`)
-                    .then(res => res.json())
-                    .then(data => {
-                        setPokeList(prev => {
-                            return { ...prev, active: true, pokeArr: chosenPoke, hiddenPoke: data }
-                        })
-                    })
-
+        let x = chosenPoke[Math.floor(Math.random() * 4)]
+        fetch(`${x.url}`)
+        .then(res => res.json())
+        .then(data => {
+            setSettings(prev => {
+                return { ...prev, active: true, pokeArr: chosenPoke, hiddenPoke: data }
             })
+        })
+
     }
 
-    const {hiddenPoke, pokeArr} = pokeList
+    function checkAnswer(name){
+        name === hiddenPoke.name ? loadPokemon() : console.log('nope')
+    }
 
     return (
         <div className='gameset'>
-            {pokeList.active && <img src={hiddenPoke.sprites.front_default} alt="" />}
+            {settings.active && <img src={hiddenPoke.sprites.front_default} alt="" />}
             <h1>Whos That Pokémon?</h1>
-            {!pokeList.active && <button onClick={loadPokemon}>Start!</button>}
-            {pokeList.pokeArr[0] && <button>{pokeArr[0].name}</button>}
-            {pokeList.pokeArr[1] && <button>{pokeArr[1].name}</button>}
-            {pokeList.pokeArr[2] && <button>{pokeArr[2].name}</button>}
-            {pokeList.pokeArr[3] && <button>{pokeArr[3].name}</button>}
+            {!settings.active && <button onClick={loadPokemon}>Start!</button>}
+            {settings.pokeArr[0] && <button onClick={()=>checkAnswer(pokeArr[0].name)}>{pokeArr[0].name}</button>}
+            {settings.pokeArr[1] && <button onClick={()=>checkAnswer(pokeArr[1].name)}>{pokeArr[1].name}</button>}
+            {settings.pokeArr[2] && <button onClick={()=>checkAnswer(pokeArr[2].name)}>{pokeArr[2].name}</button>}
+            {settings.pokeArr[3] && <button onClick={()=>checkAnswer(pokeArr[3].name)}>{pokeArr[3].name}</button>}
         </div>
     )
 }
